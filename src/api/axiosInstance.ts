da65a77233e93b179a32987d8c423a1839655bb4
manojconcept <manojveerapavu@gmail.com>
2025-02-13 15:37:36 +0530

    init

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const userAxiosInstance = axios.create({
-  baseURL: 'https://backend-template-six.vercel.app/app',
+  baseURL: 'https://backend-template-six.vercel.app/api',
   withCredentials: true, 
 });
 
